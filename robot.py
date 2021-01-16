8664941d331d2a18ef5020ee12cda31a841532ec
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2021-01-16 14:03:17 +1030

    Entropical Bumblebot Logic Quest Pro

--- robot.py
+++ robot.py
@@ -3,14 +3,21 @@ import random
 from sr.robot import *
 R = Robot()
 
-leftSpeed = 80
-rightSpeed = 80
+leftSpeed = 70
+rightSpeed = 70
+turnSpeed = 20
+counterReact = turnSpeed / 2
 
 R.motors[0].m0.power = 50
-R.motors[0].m1.power = 60
-R.sleep(3)
+R.motors[0].m1.power = 56
+R.sleep(2.5)
 R.motors[0].m0.power = 0
 R.motors[0].m1.power = 0
+R.radio.claim_territory()
+R.sleep(.1)
+R.motors[0].m0.power = 1.5
+R.motors[0].m1.power = -1.1
+R.sleep(.85)
 
 while (True):
     R.radio.claim_territory()
@@ -21,22 +28,55 @@ while (True):
             if(tx.target_info.owned_by != R.zone):
                 greatestSignal = tx.signal_strength
 
-            
     R.motors[0].m0.power = leftSpeed
     R.motors[0].m1.power = rightSpeed
     
-    if(greatestSignal > 1):
-        R.motors[0].m0.power = leftSpeed / (greatestSignal * 3 ) + 10
-        R.motors[0].m1.power = rightSpeed / (greatestSignal * 3 ) + 10
-    
+    if(greatestSignal > 2):
+        
+        R.motors[0].m0.power = 0
+        R.motors[0].m1.power = 0
+        R.sleep(0.5)
+        R.motors[0].m0.power = -turnSpeed
+        R.motors[0].m1.power = turnSpeed
+        R.sleep(0.5)
+    if(greatestSignal > greatestSignal):
+        R.motors[0].m0.power = 0
+        R.motors[0].m1.power = 0
+        R.sleep(0.5)
+        R.motors[0].m0.power = counterReact
+        R.motors[0].m1.power = -counterReact
+        R.sleep(0.5)
+
+#varied slowdown
+    if(greatestSignal > .7):
+        R.motors[0].m0.power = leftSpeed / (greatestSignal * 4 ) + 5
+        R.motors[0].m1.power = rightSpeed / (greatestSignal * 4 ) + 5
+
+
+    if(greatestSignal > .6):
+        R.motors[0].m0.power = leftSpeed / (greatestSignal * 3.5 )
+        R.motors[0].m1.power = rightSpeed / (greatestSignal * 3.5 )
+
+
+    if(greatestSignal > .5):
+        R.motors[0].m0.power = leftSpeed / (greatestSignal * 3 )
+        R.motors[0].m1.power = rightSpeed / (greatestSignal * 3 )
+
+#wall turn around
     if R.ruggeduinos[0].digital_read(2):
         print(greatestSignal)
         if(greatestSignal > 30):
             R.sleep(1)
-        R.motors[0].m0.power = random.randint(-80, -20)
-        R.motors[0].m1.power = random.randint(20, 80)
+        
+        direction = bool(random.getrandbits(1))
+        
+        if(direction):
+            R.motors[0].m0.power = -30
+            R.motors[0].m1.power = 30
+        else:
+            R.motors[0].m0.power = 30
+            R.motors[0].m1.power = -30
         R.sleep(1)
    
  
     R.sleep(0.01)
-    
