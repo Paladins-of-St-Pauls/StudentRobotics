7acfb018a04abc64eea4700b624044ef9e7a4736
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2021-02-05 22:52:59 +1030

    Module 2!

--- robot.py
+++ robot.py
@@ -1,23 +1,104 @@
 import random
 
 from sr.robot import *
-R = Robot()
+R = Robot(1)
 
 leftSpeed = 70
 rightSpeed = 70
 turnSpeed = 20
-counterReact = turnSpeed / 2
+counterReact = turnSpeed * 2
 
-R.motors[0].m0.power = 50
-R.motors[0].m1.power = 56
-R.sleep(2.5)
-R.motors[0].m0.power = 0
-R.motors[0].m1.power = 0
-R.radio.claim_territory()
-R.sleep(.1)
-R.motors[0].m0.power = 1.5
-R.motors[0].m1.power = -1.1
-R.sleep(.85)
+print(R.zone)
+
+if(R.zone == 1):
+    R.motors[0].m0.power = 56
+    R.motors[0].m1.power = 53
+    R.sleep(2.5)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.radio.claim_territory()
+    R.sleep(.3)
+   # R.motors[0].m0.power = -1.1
+   # R.motors[0].m1.power = 1.5
+   # R.sleep(.85)
+   # R.motors[0].m0.power = 0
+   # R.motors[0].m1.power = 0
+   # R.radio.claim_territory()
+   # R.sleep(.1)
+    R.motors[0].m0.power = 50
+    R.motors[0].m1.power = 50
+    R.sleep(2.35)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.radio.claim_territory()
+    R.sleep(.1)
+    #fix direction
+    R.motors[0].m0.power = 1.1
+    R.motors[0].m1.power = -1.5
+    R.sleep(.85)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.radio.claim_territory()
+    R.sleep(.1)
+    print ('**BABY DRIVER INTENSIFIES**')
+    print ('*BABY DRIVER INTENSIFIES*')
+    print ('**BABY DRIVER INTENSIFIES**')
+    print ('*BABY DRIVER INTENSIFIES*')
+    R.motors[0].m0.power = -94
+    R.motors[0].m1.power = -90
+    R.sleep(2.4)
+    #R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.sleep(.4)
+    #R.motors[0].m0.power = -30
+    #R.motors[0].m1.power = 30
+    #R.sleep(.5)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.sleep(.1)
+else:
+    R.motors[0].m0.power = 50
+    R.motors[0].m1.power = 56
+    R.sleep(2.5)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.radio.claim_territory()
+    R.sleep(.1)
+    R.motors[0].m0.power = 1.5
+    R.motors[0].m1.power = -1.1
+    R.sleep(.85)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.radio.claim_territory()
+    R.sleep(.1)
+    R.motors[0].m0.power = 50
+    R.motors[0].m1.power = 50
+    R.sleep(2.5)
+    #fix direction
+    R.motors[0].m0.power = -1.1
+    R.motors[0].m1.power = 1.5
+    R.sleep(.85)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.radio.claim_territory()
+    R.sleep(.1)
+    print ('**BABY DRIVER INTENSIFIES**')
+    print ('*BABY DRIVER INTENSIFIES*')
+    print ('**BABY DRIVER INTENSIFIES**')
+    print ('*BABY DRIVER INTENSIFIES*')
+    R.motors[0].m0.power = -90
+    R.motors[0].m1.power = -94
+    R.sleep(2.4)
+    R.motors[0].m0.power = 0
+    #R.motors[0].m1.power = -24
+    R.sleep(.4)
+    #R.motors[0].m0.power = 30
+    #R.motors[0].m1.power = -30
+    #R.sleep(.5)
+    R.motors[0].m0.power = 0
+    R.motors[0].m1.power = 0
+    R.sleep(.1)
+
 
 while (True):
     R.radio.claim_territory()
@@ -31,36 +112,81 @@ while (True):
     R.motors[0].m0.power = leftSpeed
     R.motors[0].m1.power = rightSpeed
     
+        #change back to 2 if it sucks chris 
     if(greatestSignal > 2):
-        
-        R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.sleep(0.5)
-        R.motors[0].m0.power = -turnSpeed
-        R.motors[0].m1.power = turnSpeed
-        R.sleep(0.5)
+        #R.motors[0].m0.power = 0
+        #R.motors[0].m1.power = 0
+        #R.sleep(0.5)
+        #R.motors[0].m0.power = -turnSpeed
+        #R.motors[0].m1.power = turnSpeed
+        #R.sleep(0.5)
+        if(R.zone == 1):
+            R.motors[0].m0.power = 0
+            R.motors[0].m1.power = 0
+            R.sleep(0.5)
+            R.motors[0].m0.power = turnSpeed
+            R.motors[0].m1.power = -turnSpeed
+            R.sleep(0.5)
+        else:
+            R.motors[0].m0.power = 0
+            R.motors[0].m1.power = 0
+            R.sleep(0.5)
+            R.motors[0].m0.power = -turnSpeed
+            R.motors[0].m1.power = turnSpeed
+            R.sleep(0.5)
     if(greatestSignal > greatestSignal):
-        R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.sleep(0.5)
-        R.motors[0].m0.power = counterReact
-        R.motors[0].m1.power = -counterReact
-        R.sleep(0.5)
+        #R.motors[0].m0.power = 0
+        #R.motors[0].m1.power = 0
+        #R.sleep(0.5)
+        #R.motors[0].m0.power = counterReact
+        #R.motors[0].m1.power = -counterReact
+        #R.sleep(0.5)
+        if(R.zone == 1):
+            R.motors[0].m0.power = 0
+            R.motors[0].m1.power = 0
+            R.sleep(0.5)
+            R.motors[0].m0.power = -counterReact
+            R.motors[0].m1.power = counterReact
+            R.sleep(0.5)
+        else:
+            R.motors[0].m0.power = 0
+            R.motors[0].m1.power = 0
+            R.sleep(0.5)
+            R.motors[0].m0.power = counterReact
+            R.motors[0].m1.power = -counterReact
 
 #varied slowdown
     if(greatestSignal > .7):
-        R.motors[0].m0.power = leftSpeed / (greatestSignal * 4 ) + 5
-        R.motors[0].m1.power = rightSpeed / (greatestSignal * 4 ) + 5
+        #R.motors[0].m0.power = leftSpeed / (greatestSignal * 4 ) + 5
+        #R.motors[0].m1.power = rightSpeed / (greatestSignal * 4 ) + 5
+        if(R.zone == 1):
+            R.motors[0].m1.power = leftSpeed / (greatestSignal * 4 ) + 5
+            R.motors[0].m0.power = rightSpeed / (greatestSignal * 4 ) + 5
+        else:
+            R.motors[0].m0.power = leftSpeed / (greatestSignal * 4 ) + 5
+            R.motors[0].m1.power = rightSpeed / (greatestSignal * 4 ) + 5
 
 
-    if(greatestSignal > .6):
-        R.motors[0].m0.power = leftSpeed / (greatestSignal * 3.5 )
-        R.motors[0].m1.power = rightSpeed / (greatestSignal * 3.5 )
+    #if(greatestSignal > .6):
+        #R.motors[0].m0.power = leftSpeed / (greatestSignal * 3.5 )
+        #R.motors[0].m1.power = rightSpeed / (greatestSignal * 3.5 )
+        if(R.zone == 1):
+            R.motors[0].m1.power = leftSpeed / (greatestSignal * 3.5 )
+            R.motors[0].m0.power = rightSpeed / (greatestSignal * 3.5 )
+        else:
+            R.motors[0].m0.power = leftSpeed / (greatestSignal * 3.5 )
+            R.motors[0].m1.power = rightSpeed / (greatestSignal * 3.5 )
 
 
     if(greatestSignal > .5):
-        R.motors[0].m0.power = leftSpeed / (greatestSignal * 3 )
-        R.motors[0].m1.power = rightSpeed / (greatestSignal * 3 )
+        #R.motors[0].m0.power = leftSpeed / (greatestSignal * 3 )
+        #R.motors[0].m1.power = rightSpeed / (greatestSignal * 3 )
+        if(R.zone == 1):
+            R.motors[0].m1.power = leftSpeed / (greatestSignal * 3 )
+            R.motors[0].m0.power = rightSpeed / (greatestSignal * 3 )
+        else:
+            R.motors[0].m0.power = leftSpeed / (greatestSignal * 3 )
+            R.motors[0].m1.power = rightSpeed / (greatestSignal * 3 )
 
 #wall turn around
     if R.ruggeduinos[0].digital_read(2):
@@ -70,12 +196,12 @@ while (True):
         
         direction = bool(random.getrandbits(1))
         
-        if(direction):
-            R.motors[0].m0.power = -(random.randint(25, 35))
-            R.motors[0].m1.power = (random.randint(25, 35))
-        else:
+        if(R.zone == 1):
             R.motors[0].m0.power = (random.randint(25, 35))
             R.motors[0].m1.power = -(random.randint(25, 35))
+        else:
+            R.motors[0].m0.power = -(random.randint(25, 35))
+            R.motors[0].m1.power = (random.randint(25, 35))
         R.sleep(1)
    
  
