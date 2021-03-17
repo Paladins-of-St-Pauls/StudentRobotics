a48daacce25d0d193e05aec8db86d70c80009d8a
Caleb Rodgers <24555386+calebrodgers@users.noreply.github.com>
2021-03-17 19:02:16 +1030

    Continued work on heading correction

--- robot.py
+++ robot.py
@@ -1,321 +1,110 @@
-import random
-import pprint
+from sr.robot import *
+import statistics
 import math
 from collections import defaultdict,deque
 
-from sr.robot import *
-R = Robot(1)
+R = Robot()
 
+m_left = R.motors[0].m0
+m_right = R.motors[0].m1
 
-leftSpeed = 70
-rightSpeed = 70
-turnSpeed = 20
-counterReact = turnSpeed * 2
+def set_power(left, right):
+    m_left.power = left
+    m_right.power = right
 
-print(R.zone)
-if False:
-    if(R.zone == 1):
-        R.motors[0].m0.power = 56
-        R.motors[0].m1.power = 53
-        R.sleep(2.5)
-        R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.radio.claim_territory()
-        R.sleep(.3)
-    # R.motors[0].m0.power = -1.1
-    # R.motors[0].m1.power = 1.5
-    # R.sleep(.85)
-    # R.motors[0].m0.power = 0
-    # R.motors[0].m1.power = 0
-    # R.radio.claim_territory()
-    # R.sleep(.1)
-        R.motors[0].m0.power = 50
-        R.motors[0].m1.power = 50
-        R.sleep(2.35)
-        R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.radio.claim_territory()
-        R.sleep(.1)
-        #fix direction
-        R.motors[0].m0.power = 1.1
-        R.motors[0].m1.power = -1.5
-        R.sleep(.85)
-        R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.radio.claim_territory()
-        R.sleep(.1)
-        print ('**BABY DRIVER INTENSIFIES**')
-        print ('*BABY DRIVER INTENSIFIES*')
-        print ('**BABY DRIVER INTENSIFIES**')
-        print ('*BABY DRIVER INTENSIFIES*')
-        R.motors[0].m0.power = -94
-        R.motors[0].m1.power = -90
-        R.sleep(2.4)
-        #R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.sleep(.4)
-        #R.motors[0].m0.power = -30
-        #R.motors[0].m1.power = 30
-        #R.sleep(.5)
-        R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.sleep(.1)
+def get_heading(n=5):
+    heading = 0
+    for i in range(0,n):
+        heading += R.compass.get_heading()
+    return heading/n * (360/math.tau)
+
+def turn(degrees):
+    radians = degrees * (math.tau/360)
+    if radians != 0:
+        per_tenth = 0.43737389828257306 
+        sleep_time = math.fabs(radians)/per_tenth/10
+        power = 75 
+        p = math.copysign(power,radians)
     else:
-        R.motors[0].m0.power = 50
-        R.motors[0].m1.power = 56
-        R.sleep(2.5)
-        R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.radio.claim_territory()
-        R.sleep(.1)
-        R.motors[0].m0.power = 1.5
-        R.motors[0].m1.power = -1.1
-        R.sleep(.85)
-        R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.radio.claim_territory()
-        R.sleep(.1)
-        R.motors[0].m0.power = 50
-        R.motors[0].m1.power = 50
-        R.sleep(2.5)
-        #fix direction
-        R.motors[0].m0.power = -1.1
-        R.motors[0].m1.power = 1.5
-        R.sleep(.85)
-        R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.radio.claim_territory()
-        R.sleep(.1)
-        print ('**BABY DRIVER INTENSIFIES**')
-        print ('*BABY DRIVER INTENSIFIES*')
-        print ('**BABY DRIVER INTENSIFIES**')
-        print ('*BABY DRIVER INTENSIFIES*')
-        R.motors[0].m0.power = -90
-        R.motors[0].m1.power = -94
-        R.sleep(2.4)
-        R.motors[0].m0.power = 0
-        #R.motors[0].m1.power = -24
-        R.sleep(.4)
-        #R.motors[0].m0.power = 30
-        #R.motors[0].m1.power = -30
-        #R.sleep(.5)
-        R.motors[0].m0.power = 0
-        R.motors[0].m1.power = 0
-        R.sleep(.1)
+        p = 0
+    print(f"TURN power[{p}] sleep[{sleep_time}]")
+    set_power(p,-p)
+    R.sleep(sleep_time)
 
-last_seen_transmitters=deque(maxlen=3)
-def are_we_stuck(transmitters):
-    last_seen_transmitters.append(transmitters)
+def move(power, distance):
+    per_distance = 2
+    sleep_time = math.fabs(distance/per_distance)
+    p = math.copysign(power,distance)
+    print(f"MOVE power[{p}] sleep[{sleep_time}]")
+    set_power(p,p)
+    R.sleep(sleep_time)
 
-    if len(last_seen_transmitters) < 3:
-        return False
+def stop(sleep_time=0.5):
+    set_power(0,0)
+    R.sleep(sleep_time)
 
-    stations=defaultdict(list)
-    for transmitters in last_seen_transmitters:
-        for tx in transmitters:
-            stations[tx.target_info.station_code].append(tx.signal_strength)
+tx_depends = defaultdict(list)
+#TODO record the dependecies of the towers
 
-    # if the number of items in each list is not the same as the length of the deque then
-    # we have a changing number of station codes
-    for key,value in stations.items():
-        if len(value) < len(last_seen_transmitters):
-            return False
-
-    # now check the strengths are all within a certain percentage
-
-    for station_code,strengths in stations.items():
-        strength_avg = sum(strengths)/len(strengths)
-        if max(strengths)/strength_avg > 1.2:
-            return False
-        if min(strengths)/strength_avg < 0.8:
-            return False
-    return True
-
-tx_status = {}
+tx_status = defaultdict(dict)
 for station_code in StationCode:
-    tx_status[station_code] = {}
-
-R.motors[0].m0.power = 5 
-R.motors[0].m1.power = 5 
-while (True):
+    tx_status[station_code]['tx'] = None
+    tx_status[station_code]['strength'] = None
+    tx_status[station_code]['bearing'] = None
     
-
-    greatestSignal = 0
-    greatestSignalNotMine = 0
+def sweep():
     transmitters = R.radio.sweep()
-
-    pprint.pprint(transmitters)
-    if are_we_stuck(transmitters):
-        print("WE ARE STUCK")
-
-    
-
-    #  [<Target: info=TargetInfo(station_code=<StationCode.PN: 'PN'>, owned_by=None, locked=False), bearing=-1.6641014529817628, strength=1.5047800429345402>,
-    #   <Target: info=TargetInfo(station_code=<StationCode.BG: 'BG'>, owned_by=None, locked=False), bearing=0.6825763859138019, strength=0.5934829139807503>]
-    closest_tx = None
-    closest_not_mine = None
+    # Keep the latest sweep code in 'latest'
+    tx_status['latest'] = []
     for tx in transmitters:
-        tx_status[tx.target_info.station_code]['tx'] = tx
-        if(tx.signal_strength > greatestSignal):
-            closest_tx = tx
-            greatestSignal = tx.signal_strength
-        if (tx.target_info.owned_by != R.zone):
-            if(tx.signal_strength > greatestSignalNotMine):
-                closest_not_mine = tx
-                greatestSignalNotMine = tx.signal_strength                
+        station_code = tx.target_info.station_code
+        tx_status['latest'].append(station_code)
+        # copy the full struct
+        tx_status[station_code]['tx'] = tx
+        # and the individual values
+        tx_status[station_code]['strength'] = tx.signal_strength
+        tx_status[station_code]['bearing'] = tx.bearing
+        tx_status[station_code]['owner'] = tx.target_info.owned_by
+        tx_status[station_code]['locked'] = tx.target_info.locked
 
-    if closest_tx.target_info.owned_by != R.zone:
-        R.radio.claim_territory()
-    
-    robot_bearing = R.compass.get_heading()
-    if robot_bearing < 0:
-        robot_bearing += 2 * math.pi
+def correct_heading(degrees, variance=2):
+    heading = get_heading()
+    print(f"Current heading: {heading}")
+    diff = heading - degrees
+    if math.fabs(diff) < variance:
+        print("No need to correct")
+        return
+    print(f"Correcting by {diff} degrees.")
+    turn(-diff)
+    stop()
+    return
 
-    if closest_not_mine:
-        tower_bearing = closest_not_mine.bearing
-        if tower_bearing < 0:
-            tower_bearing += 2 * math.pi
-
-        diff = robot_bearing - tower_bearing
-        print(f"diff={diff}")
-        left_power =  20 - (50*(math.sin(diff)))
-        right_power = 20 + (50*math.sin(diff))
-        print(f"left={left_power} , right={right_power}")
-        R.motors[0].m0.power = left_power
-        R.motors[0].m1.power = right_power
-    else:
-        R.motors[0].m0.power = leftSpeed + random.randint(-30,30)
-        R.motors[0].m1.power = rightSpeed + random.randint(-30,30)
-
-
-
-
-
-while (True):
-    
-    greatestSignal = 0
-    greatestSignalNotMine = 0
-    transmitters = R.radio.sweep()
-
-    pprint.pprint(transmitters)
-
-
-    #  [<Target: info=TargetInfo(station_code=<StationCode.PN: 'PN'>, owned_by=None, locked=False), bearing=-1.6641014529817628, strength=1.5047800429345402>,
-    #   <Target: info=TargetInfo(station_code=<StationCode.BG: 'BG'>, owned_by=None, locked=False), bearing=0.6825763859138019, strength=0.5934829139807503>]
-    closest_tx = None
-    closest_not_mine = None
-    for tx in transmitters:
-        tx_status[tx.target_info.station_code]['tx'] = tx
-        if(tx.signal_strength > greatestSignal):
-            closest_tx = tx
-            greatestSignal = tx.signal_strength
-        if (tx.target_info.owned_by != R.zone):
-            if(tx.signal_strength > greatestSignalNotMine):
-                closest_not_mine = tx
-                greatestSignalNotMine = tx.signal_strength                
-
-    if closest_tx.target_info.owned_by != R.zone:
-        R.radio.claim_territory()
-    
-    if closest_not_mine:
-        R
-    else:
-        pass
-
-    R.motors[0].m0.power = leftSpeed
-    R.motors[0].m1.power = rightSpeed
-    
-        #change back to 2 if it sucks chris 
-    if(greatestSignal > 2):
-        #R.motors[0].m0.power = 0
-        #R.motors[0].m1.power = 0
-        #R.sleep(0.5)
-        #R.motors[0].m0.power = -turnSpeed
-        #R.motors[0].m1.power = turnSpeed
-        #R.sleep(0.5)
-        if(R.zone == 1):
-            R.motors[0].m0.power = 0
-            R.motors[0].m1.power = 0
-            R.sleep(0.5)
-            R.motors[0].m0.power = turnSpeed
-            R.motors[0].m1.power = -turnSpeed
-            R.sleep(0.5)
-        else:
-            R.motors[0].m0.power = 0
-            R.motors[0].m1.power = 0
-            R.sleep(0.5)
-            R.motors[0].m0.power = -turnSpeed
-            R.motors[0].m1.power = turnSpeed
-            R.sleep(0.5)
-    if(greatestSignal > greatestSignal):
-        #R.motors[0].m0.power = 0
-        #R.motors[0].m1.power = 0
-        #R.sleep(0.5)
-        #R.motors[0].m0.power = counterReact
-        #R.motors[0].m1.power = -counterReact
-        #R.sleep(0.5)
-        if(R.zone == 1):
-            R.motors[0].m0.power = 0
-            R.motors[0].m1.power = 0
-            R.sleep(0.5)
-            R.motors[0].m0.power = -counterReact
-            R.motors[0].m1.power = counterReact
-            R.sleep(0.5)
-        else:
-            R.motors[0].m0.power = 0
-            R.motors[0].m1.power = 0
-            R.sleep(0.5)
-            R.motors[0].m0.power = counterReact
-            R.motors[0].m1.power = -counterReact
-
-#varied slowdown
-    if(greatestSignal > .7):
-        #R.motors[0].m0.power = leftSpeed / (greatestSignal * 4 ) + 5
-        #R.motors[0].m1.power = rightSpeed / (greatestSignal * 4 ) + 5
-        if(R.zone == 1):
-            R.motors[0].m1.power = leftSpeed / (greatestSignal * 4 ) + 5
-            R.motors[0].m0.power = rightSpeed / (greatestSignal * 4 ) + 5
-        else:
-            R.motors[0].m0.power = leftSpeed / (greatestSignal * 4 ) + 5
-            R.motors[0].m1.power = rightSpeed / (greatestSignal * 4 ) + 5
-
-
-    #if(greatestSignal > .6):
-        #R.motors[0].m0.power = leftSpeed / (greatestSignal * 3.5 )
-        #R.motors[0].m1.power = rightSpeed / (greatestSignal * 3.5 )
-        if(R.zone == 1):
-            R.motors[0].m1.power = leftSpeed / (greatestSignal * 3.5 )
-            R.motors[0].m0.power = rightSpeed / (greatestSignal * 3.5 )
-        else:
-            R.motors[0].m0.power = leftSpeed / (greatestSignal * 3.5 )
-            R.motors[0].m1.power = rightSpeed / (greatestSignal * 3.5 )
-
-
-    if(greatestSignal > .5):
-        #R.motors[0].m0.power = leftSpeed / (greatestSignal * 3 )
-        #R.motors[0].m1.power = rightSpeed / (greatestSignal * 3 )
-        if(R.zone == 1):
-            R.motors[0].m1.power = leftSpeed / (greatestSignal * 3 )
-            R.motors[0].m0.power = rightSpeed / (greatestSignal * 3 )
-        else:
-            R.motors[0].m0.power = leftSpeed / (greatestSignal * 3 )
-            R.motors[0].m1.power = rightSpeed / (greatestSignal * 3 )
-
-#wall turn around
-    if R.ruggeduinos[0].digital_read(2):
-        print(greatestSignal)
-        if(greatestSignal > 30):
-            R.sleep(1)
-        
-        direction = bool(random.getrandbits(1))
-        
-        if(R.zone == 1):
-            R.motors[0].m0.power = (random.randint(25, 35))
-            R.motors[0].m1.power = -(random.randint(25, 35))
-        else:
-            R.motors[0].m0.power = -(random.randint(25, 35))
-            R.motors[0].m1.power = (random.randint(25, 35))
-        R.sleep(1)
-   
- 
-    R.sleep(0.01)
+# First part by dead-reckoning
+print(f"heading = {get_heading(1000)}")
+move(75,1.4)
+stop()
+print(f"heading = {get_heading(1000)}")
+turn(-40)
+stop()
+print(f"heading before correction = {get_heading(1000)}")
+correct_heading(116)
+print(f"heading = {get_heading(1000)}")
+move(75,2.2)
+stop(1.0)
+correct_heading(116)
+R.radio.claim_territory()
+print(f"heading = {get_heading(1000)}")
+move(100,2.6)
+stop(1.5)
+correct_heading(116)
+R.radio.claim_territory()
+print(f"heading = {get_heading(1000)}")
+move(100,-5.5)
+print(f"heading = {get_heading(1000)}")
+stop(1.5)
+correct_heading(180)
+correct_heading(180)
+correct_heading(180)
+correct_heading(180)
+correct_heading(180)
+move(100, 3)
