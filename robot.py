af5860345a138ac65715e853f735c1070761df5f
Andrew <11323044+amstu2@users.noreply.github.com>
2021-04-03 23:43:42 +1030

    Add skeleton classes

--- robot.py
+++ robot.py
@@ -68,6 +68,28 @@ station_score_dict = {
     StationCode.SF: 2,
     StationCode.BN: 2
 }
+
+station_pos_dict = {
+    StationCode.OX: [-6.6, 3],
+    StationCode.TS: [-2.75,2.75],
+    StationCode.VB: [-1.95,0.75],
+    StationCode.BG: [-4.2, 0],
+    StationCode.EY: [-1.95,-0.75],
+    StationCode.PN: [-4.2,-1.8],
+    StationCode.TH: [-6.6,-3],
+    StationCode.PL: [0,3],
+    StationCode.BE: [0, 1.5],
+    StationCode.HA: [0,0],
+    StationCode.YT: [0,-1.5],
+    StationCode.FL: [0,-3],
+    StationCode.PO: [1.95,-0.75],
+    StationCode.SZ: [1.95,0.75],
+    StationCode.SW: [2.75,2.75],
+    StationCode.YL: [4.2,-1.8],
+    StationCode.HV: [4.2,0],
+    StationCode.SF: [6.6,-3],
+    StationCode.BN: [6.6,3]    
+}
         
 class MotorController:
     def __init__(self, robot, l_motor, r_motor) -> None:     
@@ -108,10 +130,14 @@ class SensorController:
     def set_led(self, led, state) -> None:
         self.robot.ruggeduinos[0].digital_write(led, state)
 
-    def get_compass_heading(self) -> float:
-        x, _, z = self.robot.compass._compass.getValues()
-        heading = math.atan2(x, z) % math.tau
-        return heading * (360/math.tau)
+    def get_compass_heading_raw(self) -> float:
+        return self.robot.compass.get_heading()
+
+    def get_compass_heading(self,num_readings=5) -> float:
+        heading = 0
+        for i in range(0,num_readings):
+            heading += self.robot.compass.get_heading()
+        return heading/num_readings * (360/math.tau)
 
     def radio_sweep(self, tx_data):
         transmitters = self.robot.radio.sweep()
@@ -128,3 +154,60 @@ class SensorController:
             self.tx_status[station_code]['owner'] = tx.target_info.owned_by
             self.tx_status[station_code]['distance'] = signal_strength_to_distance(tx.signal_strength)
             #print(f"[{station_code}] - bearing {tx_status[station_code]['bearing']:.2f}  distance - {tx_status[station_code]['distance']:.2f}   strength - {tx_status[station_code]['strength']:.2f}") if debug_active else \
+    
+    def get_tx_status() -> dict:
+        return self.tx_status
+
+class Pose:
+    def __init__(self, robot, sc, init_x, init_y):
+        self.location = numpy.array([init_x, init_y])
+        self.robot = robot
+        self.sensor_controller = sc
+        self.heading = init_heading
+
+    def set_location(self, x, y):
+        self.location = numpy.array([x, y])
+
+    def estimate_location(self):
+        xs = []
+        ys = []
+        weights = []
+        tx_status = self.sensor_controller.get_tx_status
+        for station_code in tx_status['latest']:
+            bearing = tx_status[station_code]['bearing']
+            distance = tx_status[station_code]['distance']
+            angle = (180 + sc.get_compass_heading + bearing)%360
+            x = station_pos_dict[station_code][0] + math.sin(angle/360*math.tau)*distance
+            y = station_pos_dict[station_code][1] - math.cos(angle/360*math.tau)*distance
+            xs.append(x)
+            ys.append(y)
+            weights.append(1.0/(distance + 0.1))
+        if len(xs) < 1:
+            return
+        x = numpy.average(xs)
+        y = numpy.average(ys)
+        xw = numpy.average(xs,weights=weights)
+        yw = numpy.average(ys,weights=weights)
+        distance_moved_since_last_update = get_distance(last_robot_pos, [xw,yw])
+        set_location(xw, yw)
+        last_robot_pos_list.append(distance_moved_since_last_update)
+
+class EnvironmentMapper:
+    def __init__(self):
+        pass
+
+class WaypointController:
+    def __init__(self):
+        pass
+
+class PathFinder:
+    def __init__(self):
+        pass    
+
+class Navigator:
+    def __init__(self):
+        pass    
+            
+robot = Robot()
+mc = MotorController(robot, robot.motors[0].m0, robot.motors[0].m1)
+sc = SensorController(robot, StationCode)
